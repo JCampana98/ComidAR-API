859eb6f4431449f3eabaf617b13fafc698198632
Julian Ariel Campana <campanajulian@gmail.com>
2019-08-20 09:40:02 -0400

    Disabled Spring Scurity Login

--- src/main/java/ar/edu/um/comidar/ComidARApplication.java
+++ src/main/java/ar/edu/um/comidar/ComidARApplication.java
@@ -2,8 +2,9 @@ package ar.edu.um.comidar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ComidARApplication {
 
 	public static void main(String[] args) {
