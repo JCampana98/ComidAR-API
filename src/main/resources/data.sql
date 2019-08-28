00f84740e9dde663be6b109c4181c957b552243f
Julian Ariel Campana <campanajulian@gmail.com>
2019-08-28 16:01:25 -0400

    Fixes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,5 +6,4 @@ INSERT INTO restaurants (creation_date,description,direction,email,last_update_d
 ,('2019-08-20 19:27:50.827','Cantina/Resto/Bar','Chile 19','cantina@gmail.com','2019-08-20 19:27:50.827','Cantina a GoGo','123456789','/restaurants/5.jpg')
 ,('2019-08-20 19:28:36.200','Restaurante en frente de la rotonda','Avenida Bernardino Rivadavia 18','lamaquina@gmail.com','2019-08-20 19:28:36.200','La Maquina','123456789','/restaurants/6.jpg')
 ,('2019-08-20 19:29:21.430','Restaurante al lado del cafecito','Pellegrini 190','lasduelas@gmail.com','2019-08-20 19:29:21.430','Las Duelas','123456789','/restaurants/7.jpg')
-INSERT INTO categories ("name") VALUES ('Pastas'),('Hamburguesas'),('Pizzas') 
 ;
