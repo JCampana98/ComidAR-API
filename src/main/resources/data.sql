dc9485872f7bf798fd73f07f68129597e22ad0ab
Julian Ariel Campana <campanajulian@gmail.com>
2019-09-25 16:55:27 -0300

    Fix on data.sql

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO restaurants (creation_date,description,direction,email,last_update_date,"name",telephone, image_url) VALUES 
-('2019-08-20 19:25:16.764','Una pizzeria','Hipolito Hirigoyen xxx','lamasa@gmail.com','2019-08-20 19:25:16.764','La Masa al Plato','1232456789','/restaurants/1.jpg')
-,('2019-08-20 19:25:46.971','Una hamburguesería','Ballofet xxx','lapulperia@gmail.com','2019-08-20 19:25:46.971','La Pulperia','123456789','/restaurants/2.jpg')
-,('2019-08-20 19:26:15.706','Un restaurante familiar','Hipolito Hirigoyen y Buchardo','mickelinos@gmail.com','2019-08-20 19:26:15.706','Mickelinos','123456789','/restaurants/3.jpg')
-,('2019-08-20 19:26:45.943','Un restaurante fifi','Hipolito Hirigoyen xxxx','pettra@gmail.com','2019-08-20 19:26:45.943','Pettra','123456789','/restaurants/4.jpg')
-,('2019-08-20 19:27:50.827','Cantina/Resto/Bar','Chile 19','cantina@gmail.com','2019-08-20 19:27:50.827','Cantina a GoGo','123456789','/restaurants/5.jpg')
-,('2019-08-20 19:28:36.200','Restaurante en frente de la rotonda','Avenida Bernardino Rivadavia 18','lamaquina@gmail.com','2019-08-20 19:28:36.200','La Maquina','123456789','/restaurants/6.jpg')
-,('2019-08-20 19:29:21.430','Restaurante al lado del cafecito','Pellegrini 190','lasduelas@gmail.com','2019-08-20 19:29:21.430','Las Duelas','123456789','/restaurants/7.jpg')
-;
